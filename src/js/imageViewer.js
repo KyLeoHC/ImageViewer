2d31a4c23754cf4492f7d4776511842a315c4ebe
apple <kyleo.hc@foxmail.com>
2016-10-13 17:48:25 +0800

    v0.0.5

--- src/js/imageViewer.js
+++ src/js/imageViewer.js
@@ -1,11 +1,24 @@
 (function (window, document, undefined) {
     var touch = window.touch;
     var itemAnimationClass = 'item-animation';
+    var debugEl = query('#debug')[0];
+    var stopSwipe = false;
 
-    function getTransformStyle(scale, x, y) {
-        var strStyle = 'scale($scale) translate3d($X, $Y, 0)';
-        var style = strStyle.replace('$scale', scale + '').replace('$X', x + 'px').replace('$Y', y + 'px');
-        console.log(style);
+    function getTranslateStyle(x, y) {
+        var strStyle = 'translate3d($X, $Y, 0)';
+        var style = strStyle.replace('$X', x + 'px').replace('$Y', y + 'px');
+        return style;
+    }
+
+    function getScaleStyle(scale) {
+        var strStyle = 'scale3d($scale,$scale,1)';
+        var style = strStyle.replace(/\$scale/g, scale + '');
+        return style;
+    }
+
+    function getScaleAndTranslateStyle(scale, x, y) {
+        var strStyle = 'scale3d($scale,$scale,1) translate3d($X, $Y, 0)';
+        var style = strStyle.replace(/\$scale/g, scale + '').replace('$X', x + 'px').replace('$Y', y + 'px');
         return style;
     }
 
@@ -15,28 +28,66 @@
     }
 
     /********************* Viewer类 start***************************/
-    function Viewer(src, el, index, width) {
+    function Viewer(src, el, index, width, height) {
         this.el = el;
+        this.panelEl = el.firstElementChild;
         this.src = src;
         this.index = index;
         this.width = width;
-        this.scale = 1;
+        this.height = height;
         this.translateX = this.index * this.width;
         this.translateY = 0;
         this.currentX = 0; //当前正在移动的X轴距离(临时保存,当事件结束后,会赋值回translateX)
         this.currentY = 0; //当前正在移动的Y轴距离(临时保存,当事件结束后,会赋值回translateY)
-        this.el.style.transform = getTransformStyle(this.scale, this.translateX, this.translateY);
+        this.scale = 1;
+        this.currentScale = 1; //当前正在缩放的倍数(临时保存,当事件结束后,会赋值回scale)
+        this.translatePanelX = 0;
+        this.translatePanelY = 0;
+        this.currentPanelX = 0;
+        this.currentPanelY = 0;
+        this.el.style.transform = getTranslateStyle(this.translateX, this.translateY);
+        this._bindEvent();
     }
 
-    Viewer.prototype.init = function (displayIndex) {
+    Viewer.prototype._init = function (displayIndex, resetScale) {
         var image = query('img', this.el)[0];
         image.src = this.src;
         image.onload = function () {
-            this.scale = 1;
+            this.scale = resetScale ? 1 : this.scale;
             this.translateX = displayIndex * this.width;
-            this.translateY = -this.el.clientHeight / 2;
-            this.el.style.transform = getTransformStyle(this.scale, this.translateX, this.translateY);
+            this.translateY = -this.el.clientHeight * this.scale / 2;
+            this.translatePanelX = 0;
+            this.translatePanelY = 0;
+            this.el.style.transform = getTranslateStyle(this.translateX, this.translateY);
+            this.panelEl.style.transform = getScaleAndTranslateStyle(this.scale, 0, 0);
         }.bind(this);
+        return this;
+    };
+
+    Viewer.prototype._bindEvent = function () {
+        touch.on(this.panelEl, 'dragstart', function (event) {
+            stopSwipe = this.panelEl.clientWidth * this.scale > this.width;
+            if (stopSwipe) {
+                event.stopPropagation();
+            }
+        }.bind(this));
+        touch.on(this.panelEl, 'drag', function (event) {
+            if (stopSwipe) {
+                event.stopPropagation();
+                this.translatePanel(event.distanceX, event.distanceY);
+            }
+        }.bind(this));
+        touch.on(this.panelEl, 'dragend', function (event) {
+            if (stopSwipe) {
+                event.stopPropagation();
+                this.translatePanelEnd();
+            }
+        }.bind(this));
+    };
+
+    Viewer.prototype.addAnimation = function () {
+        this.el.classList.add(itemAnimationClass);
+        return this;
     };
 
     Viewer.prototype.removeAnimation = function () {
@@ -44,16 +95,42 @@
         return this;
     };
 
-    Viewer.prototype.scale = function (scale) {
-        scale = isNaN(scale) ? this.scale : scale;
-        this.el.style.transform = getTransformStyle(scale, this.translateX, this.translateY);
+    Viewer.prototype.pinch = function (scale) {
+        //debugEl.innerText = this.currentScale + '/' + scale;
+        this.currentScale = isNaN(scale) ? this.scale : (scale + this.scale);
+        if (this.currentScale > 1) {
+            this.currentScale = this.currentScale > 2 ? 2 : this.currentScale;
+        } else {
+            this.currentScale = this.currentScale < 0.5 ? 0.5 : this.currentScale;
+        }
+        this.panelEl.style.transform = getScaleAndTranslateStyle(this.currentScale, this.translatePanelX, this.translatePanelY);
+        return this;
+    };
+
+    Viewer.prototype.pinchEnd = function (scale) {
+        this.scale = isNaN(scale) ? this.currentScale : scale;
+        return this;
+    };
+
+    Viewer.prototype.translatePanel = function (translatePanelX, translatePanelY) {
+        this.currentPanelX = isNaN(translatePanelX) ? this.translatePanelX : ((translatePanelX + this.translatePanelX) / this.scale);
+        this.currentPanelY = isNaN(translatePanelY) || this.el.clientHeight * this.scale <= this.height
+            ? this.translatePanelY : ((translatePanelY + this.translatePanelY) / this.scale);
+        this.panelEl.style.transform = getScaleAndTranslateStyle(this.scale, this.currentPanelX, this.currentPanelY);
+        return this;
+    };
+
+    Viewer.prototype.translatePanelEnd = function (translatePanelX, translatePanelY) {
+        this.translatePanelX = isNaN(translatePanelX) ? this.currentPanelX : translatePanelX;
+        this.translatePanelY = isNaN(translatePanelY) ? this.currentPanelY : translatePanelY;
         return this;
     };
 
     Viewer.prototype.translate = function (translateX, translateY) {
         this.currentX = isNaN(translateX) ? this.translateX : (translateX + this.translateX);
-        this.currentY = isNaN(translateY) ? this.translateY : (translateY + this.translateY);
-        this.el.style.transform = getTransformStyle(this.scale, this.currentX, this.currentY);
+        this.currentY = isNaN(translateY) || this.el.clientHeight * this.scale < this.height
+            ? this.translateY : (translateY + this.translateY);
+        this.el.style.transform = getTranslateStyle(this.currentX, this.currentY);
         return this;
     };
 
@@ -64,17 +141,17 @@
     };
 
     Viewer.prototype.swipeToPrev = function () {
-        this.init(-1);
+        this.addAnimation()._init(-1, true);
         return this;
     };
 
-    Viewer.prototype.swipeToCurrent = function () {
-        this.init(0);
+    Viewer.prototype.swipeToCurrent = function (needReset) {
+        this.addAnimation()._init(0, needReset);
         return this;
     };
 
     Viewer.prototype.swipeToNext = function () {
-        this.init(1);
+        this.addAnimation()._init(1, true);
         return this;
     };
     /********************* Viewer类 end***************************/
@@ -82,14 +159,15 @@
     /********************* ImageViewer类 start***************************/
     function ImageViewer(images) {
         this.el = query('.image-viewer')[0];
-        this.itemList = query('.item');
+        this.itemList = query('.viewer');
         this.width = this.el.clientWidth;
         this.height = this.el.clientHeight;
         this.images = images;
         this.viewers = [];
         this.currentIndex = 1;
-        this.init();
-        this.bindEvent();
+        this.scaleStart = 1;
+        this._init();
+        this._bindEvent();
     }
 
     ImageViewer.prototype.swipeInByIndex = function (index) {
@@ -104,22 +182,34 @@
         nextViewer && nextViewer.swipeToNext();
     };
 
-    ImageViewer.prototype.init = function () {
+    ImageViewer.prototype._init = function () {
         this.itemList.forEach(function (item, index) {
-            this.viewers.push(new Viewer(this.images[index], item, index, this.width));
+            this.viewers.push(new Viewer(this.images[index], item, index, this.width, this.height));
         }.bind(this));
         this.swipeInByIndex();
     };
 
-    ImageViewer.prototype.bindEvent = function () {
-        touch.on(this.el, 'dragstart', '.item', this.dealWithMoveActionStart.bind(this));
-        touch.on(this.el, 'drag', '.item', this.dealWithMoveAction.bind(this));
-        touch.on(this.el, 'dragend', '.item', this.dealWithMoveActionEnd.bind(this));
-        touch.on(this.el, 'pinch', '.item', this.dealWithScaleAction.bind(this));
-        touch.on(this.el, 'pinchend', '.item', this.dealWithScaleActionEnd.bind(this));
+    ImageViewer.prototype._bindEvent = function () {
+        touch.config = {
+            swipeTime: 50,             //触发swipe事件的最大时长
+            swipeMinDistance: 5,       //swipe移动最小距离
+            swipeFactor: 1
+        };
+        touch.on(this.el, 'doubletap', this.reset.bind(this));
+        touch.on(this.el, 'dragstart', '.viewer', this.dealWithMoveActionStart.bind(this));
+        touch.on(this.el, 'drag', '.viewer', this.dealWithMoveAction.bind(this));
+        touch.on(this.el, 'dragend', '.viewer', this.dealWithMoveActionEnd.bind(this));
+        touch.on(this.el, 'pinchstart', '.viewer', this.dealWithScaleActionStart.bind(this));
+        touch.on(this.el, 'pinch', '.viewer', this.dealWithScaleAction.bind(this));
+        touch.on(this.el, 'pinchend', '.viewer', this.dealWithScaleActionEnd.bind(this));
+    };
+
+    ImageViewer.prototype.reset = function () {
+        this.getCurrentViewer().swipeToCurrent(true);
     };
 
     ImageViewer.prototype.dealWithMoveActionStart = function () {
+        if (stopSwipe)return;
         var prevViewer = this.getPrevViewer(),
             currentViewer = this.getCurrentViewer(),
             nextViewer = this.getNextViewer();
@@ -130,31 +220,44 @@
     };
 
     ImageViewer.prototype.dealWithMoveAction = function (event) {
+        if (stopSwipe)return;
         var prevViewer = this.getPrevViewer(),
             currentViewer = this.getCurrentViewer(),
             nextViewer = this.getNextViewer();
 
-        prevViewer && prevViewer.translate(event.distanceX, event.distanceY);
+        prevViewer && prevViewer.translate(event.distanceX);
         currentViewer && currentViewer.translate(event.distanceX, event.distanceY);
-        nextViewer && nextViewer.translate(event.distanceX, event.distanceY);
+        nextViewer && nextViewer.translate(event.distanceX);
     };
 
     ImageViewer.prototype.dealWithMoveActionEnd = function (event) {
+        if (stopSwipe)return;
+        var distanceX = event.distanceX, index;
         var prevViewer = this.getPrevViewer(),
-            currentViewer = this.getCurrentViewer(),
             nextViewer = this.getNextViewer();
+        console.log(event);
+        if (Math.abs(distanceX) < this.width / 3) {
+            index = undefined;
+        } else if (distanceX > 0) {
+            index = prevViewer ? prevViewer.index : undefined;
+        } else {
+            index = nextViewer ? nextViewer.index : undefined;
+        }
+        this.swipeInByIndex(index);
+    };
 
-        prevViewer && prevViewer.translateEnd();
-        currentViewer && currentViewer.translateEnd();
-        nextViewer && nextViewer.translateEnd();
+    ImageViewer.prototype.dealWithScaleActionStart = function (event) {
+        //console.log(event);
+        this.scaleStart = event.scale;
     };
 
     ImageViewer.prototype.dealWithScaleAction = function (event) {
-
+        //console.log(this.getCurrentViewer());
+        this.getCurrentViewer().pinch(event.scale - this.scaleStart);
     };
 
     ImageViewer.prototype.dealWithScaleActionEnd = function (event) {
-
+        this.getCurrentViewer().pinchEnd();
     };
 
     ImageViewer.prototype.getPrevViewer = function () {
