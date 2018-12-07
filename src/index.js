98116ecc728432bf6e07a17580e4ee20c00ca02f
apple <kyleo.hc@foxmail.com>
2018-12-07 12:23:54 +0800

    fix: reference error(close #2)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ImageViewer from './core/imageViewer';
 import css from './css/index.styl';
 
 /* eslint no-undef:0 */
-ImageViewer.version = __VERSION__;
+try {
+    ImageViewer.version = __VERSION__;
+} catch (e) {
+}
 
 export default ImageViewer;
